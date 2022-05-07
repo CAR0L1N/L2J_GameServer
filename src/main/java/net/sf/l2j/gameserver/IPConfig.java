7d63c447d8082117f466fafea9fcf2deb69246de
Asier Paz <asier@championgames.com>
2022-05-07 17:17:35 +0200

    Add support for ipconfig.xml file to Interlude branch
    Just copy-pasted the relevant parts from the master branch related to
    the ipconfig.xml usage
    Added Maven Wrapper

--- src/main/java/net/sf/l2j/gameserver/IPConfig.java
+++ src/main/java/net/sf/l2j/gameserver/IPConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2004-2020 L2J Server
+ * Copyright © 2004-2022 L2J Server
  * 
  * This file is part of L2J Server.
  * 
@@ -18,7 +18,13 @@
  */
 package net.sf.l2j.gameserver;
 
+import net.sf.l2j.gameserver.util.IXmlReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Inet6Address;
@@ -39,26 +45,70 @@ import java.util.stream.IntStream;
  * @author Zoey76
  * @version 2.6.1.0
  */
-public class IPConfig {
+public class IPConfig implements IXmlReader {
 	
 	private static final Logger LOG = Logger.getLogger(IPConfig.class.getName());
+	
+	private static final String IP_CONFIG_FILE = "./config/ipconfig.xml";
 	
 	private final List<String> _subnets = new ArrayList<>(5);
 	
 	private final List<String> _hosts = new ArrayList<>(5);
 	
-	protected IPConfig() {
-		LOG.info("Using automatic network configuration.");
-		
-		String externalIp = "127.0.0.1";
+	public IPConfig() {
+		load();
+	}
+	
+	@Override
+	public void load() {
+		final File f = new File(IP_CONFIG_FILE);
+		if (f.exists()) {
+			LOG.info("Using existing ipconfig.xml.");
+			parseFile(new File(IP_CONFIG_FILE));
+		} else {
+			LOG.info("Using automatic network configuration.");
+			autoIpConfig();
+		}
+	}
+
+	@Override
+	public void parseDocument(Document doc) {
+		NamedNodeMap attrs;
+		for (Node n = doc.getFirstChild(); n != null; n = n.getNextSibling()) {
+			if ("gameserver".equalsIgnoreCase(n.getNodeName())) {
+				for (Node d = n.getFirstChild(); d != null; d = d.getNextSibling()) {
+					if ("define".equalsIgnoreCase(d.getNodeName())) {
+						attrs = d.getAttributes();
+						_subnets.add(attrs.getNamedItem("subnet").getNodeValue());
+						_hosts.add(attrs.getNamedItem("address").getNodeValue());
+						
+						if (_hosts.size() != _subnets.size()) {
+							LOG.warning("Failed to load " + IP_CONFIG_FILE + " file - subnets does not match server addresses.");
+						}
+					}
+				}
+				
+				Node att = n.getAttributes().getNamedItem("address");
+				if (att == null) {
+					LOG.warning("Failed to load " + IP_CONFIG_FILE + " file - default server address is missing.");
+					_hosts.add("127.0.0.1");
+				} else {
+					_hosts.add(att.getNodeValue());
+				}
+				_subnets.add("0.0.0.0/0");
+			}
+		}
+	}
+
+	protected void autoIpConfig() {
+		String externalIp;
 		try {
-			final URL autoIp = new URL("http://ip1.dynupdate.no-ip.com:8245/");
+			URL autoIp = new URL("http://ip1.dynupdate.no-ip.com:8245/");
 			try (BufferedReader in = new BufferedReader(new InputStreamReader(autoIp.openStream()))) {
 				externalIp = in.readLine();
 			}
-		}
-		catch (IOException e) {
-			LOG.warning("Failed to connect to api.externalip.net please check your internet connection using 127.0.0.1!");
+		} catch (IOException e) {
+			LOG.warning("Failed to connect to ip1.dynupdate.no-ip.com:8245 please check your internet connection using 127.0.0.1!");
 			externalIp = "127.0.0.1";
 		}
 		
@@ -100,9 +150,8 @@ public class IPConfig {
 			_hosts.add(externalIp);
 			_subnets.add("0.0.0.0/0");
 			LOG.info("Adding new subnet: 0.0.0.0/0 address: " + externalIp);
-		}
-		catch (SocketException ex) {
-			LOG.severe("Configuration failed please manually configure ipconfig.xml!");
+		} catch (SocketException e) {
+			LOG.severe("Configuration failed please manually configure ipconfig.xml");
 			System.exit(0);
 		}
 	}
